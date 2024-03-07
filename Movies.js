288ac42f754e611f25fb22cb81e992d9411f4e7c
Justin Hoang <justin.lhoang3@gmail.com>
2024-03-07 15:48:09 -0700

    Movie schema fix

--- Movies.js
+++ Movies.js
@@ -4,18 +4,18 @@ var Schema = mongoose.Schema;
 mongoose.connect(process.env.DB);
 
 // Movie schema
-const MovieSchema = new Schema({
+const MovieSchema = new mongoose.Schema({
     title: { type: String, required: true, index: true },
     releaseDate: Date,
     genre: {
-        type: String,
-        enum: [
-            'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
-        ],
+      type: String,
+      enum: [
+        'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
+      ],
     },
     actors: [{
-        actorName: String,
-        characterName: String,
+      actorName: String,
+      characterName: String,
     }],
 });
 
